c2e396c71b219a70014e64c4ea09922cea085e94
GinzburgLev <ginzburglev95@gmail.com>
2025-10-21 14:51:52 +0200

    schema v0.1 (#4)
    * schema v0.1
    
    * now visible as built-in schema
    
    * default display units
    
    * Complete recipe for the Tajine
    
    * fix for tests maybe
    
    * linting
    
    * update schema
    
    * test changes
    
    * fix Enum
    
    * linting
    
    ---------
    
    Co-authored-by: Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
    Co-authored-by: u-gajera <gajera.uday.1@gmail.com>

--- src/nomad_tajine_plugin/schema_packages/__init__.py
+++ src/nomad_tajine_plugin/schema_packages/__init__.py
@@ -2,7 +2,7 @@ from nomad.config.models.plugins import SchemaPackageEntryPoint
 from pydantic import Field
 
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+class TajineSchemaPackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
@@ -11,7 +11,7 @@ class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
+schema_tajine_entry_point = TajineSchemaPackageEntryPoint(
+    name='TajineSchemaPackage',
+    description='Tajine schema package entry point configuration.',
 )
